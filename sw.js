a87b7f44b9e79e3330c3af1a471db0dbb03012f1
Levi-Rand <levi.mcrand@gmail.com>
2025-12-24 15:39:55 -0500

    Venture Chords
    This is the full app

---
+++ sw.js
@@ -0,0 +1,70 @@
+const CACHE_NAME = 'venture-pwa-v1766586912461';
+
+const PRECACHE_URLS = [
+  "./assets/index-BB8A_qJQ.js",
+  "./assets/index-ROwWVSqn.css",
+  "./favicon.png",
+  "./icon-192.svg",
+  "./icon-512.svg",
+  "./index.html",
+  "./manifest.json",
+  "./static-dist.tar.gz"
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(PRECACHE_URLS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name.startsWith('venture-pwa-') && name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+  const url = new URL(request.url);
+
+  if (request.method !== 'GET') {
+    return;
+  }
+
+  if (url.origin !== location.origin) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      
+      return fetch(request).then((networkResponse) => {
+        if (networkResponse.ok) {
+          const responseClone = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(request, responseClone);
+          });
+        }
+        return networkResponse;
+      }).catch(() => {
+        if (request.mode === 'navigate') {
+          return caches.match('/index.html');
+        }
+        return new Response('Offline', { status: 503 });
+      });
+    })
+  );
+});
